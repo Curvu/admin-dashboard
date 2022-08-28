4c25f20b6a7b5d89966fa05313e7f2ee7ce361a3
Curvu <rodrigues.filipealexandre2003@gmail.com>
2022-08-28 12:29:19 +0100

    function to get the repos

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-// Admin dashboard
+const projects = document.querySelector('.projects')
+// console.log(projects)
+
+const repos = []
+
+
+const getData = async () => {
+    const request = await fetch('https://api.github.com/users/curvu/repos')
+    const data = await request.json()
+    return data
+}
+
+getData()
+.then(data => {
+    for(var i = 0; i < data.length; i++) {
+        if (data[i].has_pages == true) {
+            repos.push(
+                {
+                    name: data[i].name,
+                    git: data[i].url,
+                    page: `https://curvu.github.io/${data[i].name}/`
+                }
+            )
+        }
+    }
+})
+
+console.log(repos)
